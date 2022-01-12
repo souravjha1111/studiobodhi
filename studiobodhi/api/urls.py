6f4afb762d7236e092a2be7cf1fcb2ef53b25fdd
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-01-13 00:17:24 +0530

    Update urls.py

--- studiobodhi/api/urls.py
+++ studiobodhi/api/urls.py
@@ -2,11 +2,8 @@ from django.conf.urls import url, include
 from api import views
 
 urlpatterns = [
-    url('photo-url', views.get_post_pic, name='getPhotoUrl'),
-    url('delete-photo-url/(?P<pk>[0-9]+)', views.deletephotourl.as_view(), name='delPhotoUrl'),
-    url('blog-data', views.blogdata, name='blogData'),
-    url('delete-blog-data/(?P<pk>[0-9]+)', views.deleteblogdata.as_view(), name='delblogData'),
-    url('contact-save', views.contactsave, name='contactData'),
-    url('delete-contact-save/(?P<pk>[0-9]+)', views.deletecontactsave.as_view(), name='delblogData'),
+    url('photo-url/(?P<pk>[0-9]+)', views.get_post_pic, name='getPhotoUrl'),
+    url('blog-data/(?P<pk>[0-9]+)', views.blogdata, name='blogData'),
+    url('contact-save/(?P<pk>[0-9]+)', views.contactsave, name='contactData'),
 
 ]
