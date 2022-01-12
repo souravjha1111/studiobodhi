7b976552ae7b0fee5022d39bfed4929252e959e9
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-01-13 00:17:03 +0530

    Update views.py

--- studiobodhi/api/views.py
+++ studiobodhi/api/views.py
@@ -1,19 +1,16 @@
 from django.http import response
 from .serializers import blogSerializer, urlSerializer, contactSerializer
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from .models import blogMOdel, urlModel, contactModel
 from rest_framework.decorators import api_view
-from django.shortcuts import render
 from django.views.decorators.cache import never_cache
-from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 @never_cache
 @csrf_exempt
-def get_post_pic(request):
+def get_post_pic(request, pk):
     if request.method == 'GET':
         orders = urlModel.objects.all()
         serializer = urlSerializer(orders,many = True)
@@ -26,18 +23,16 @@ def get_post_pic(request):
         else:
             return Response(serializer.errors)
 
+    elif request.method == 'DELETE':
+        urlModel.objects.get(pk=pk).delete()
+        return Response(status = 200)
 
-class deletephotourl(APIView):
-    def delete(self,request, pk):
-        urlModel.objects.get(pk =pk).delete()
-        return HttpResponse('')
 
-##################################################################################################################################
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 @never_cache
 @csrf_exempt
-def blogdata(request):
+def blogdata(request, pk):
     if request.method == 'GET':
         orders = blogMOdel.objects.all()
         serializer = blogSerializer(orders,many = True)
@@ -46,23 +41,20 @@ def blogdata(request):
         serializer  = blogSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data)
             return Response(serializer.data)
-        
         else:
             return Response(serializer.errors)
 
+    elif request.method == 'DELETE':
+        blogMOdel.objects.get(pk=pk).delete()
+        return Response(status = 200)
 
-class deleteblogdata(APIView):
-    def delete(self,request, pk):
-        blogMOdel.objects.get(pk =pk).delete()
-        return HttpResponse('')
 
-####################################################################3
-@api_view(['GET', 'POST'])
+
+@api_view(['GET', 'POST', 'DELETE'])
 @never_cache
 @csrf_exempt
-def contactsave(request):
+def contactsave(request, pk):
     if request.method == 'GET':
         orders = contactModel.objects.all()
         serializer = contactSerializer(orders,many = True)
@@ -75,118 +67,6 @@ def contactsave(request):
         else:
             return Response(serializer.errors)
 
-
-class deletecontactsave(APIView):
-    def delete(self,request, pk):
-        contactModel.objects.get(pk =pk).delete()
-        return HttpResponse('')
-
-
-
-# # class photourl(APIView):
-# #     def post(self, request):
-# #         serializer = urlSerializer(data=request.data)
-# #         serializer.is_valid(raise_exception=True)
-# #         serializer.save()
-# #         return Response(serializer.data)
-
-
-# #     def get(self, request):
-# #         product = urlModel.objects.all()
-# #         serializer = urlSerializer(product, many =True)
-# #         return Response(serializer.data)
-
-
-# class blogdata(APIView):
-#     def post(self, request):
-#         serializer = blogSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-
-
-#     def get(self, request):
-#         product = blogMOdel.objects.all()
-#         serializer = blogSerializer(product, many =True)
-#         return Response(serializer.data)
-
-
-# class contactsave(APIView):
-#     def post(self, request):
-#         serializer = contactSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data)
-
-
-#     def get(self, request):
-#         product = contactModel.objects.all()
-#         serializer = contactSerializer(product, many =True)
-#         return Response(serializer.data)
-
-
-# class delphotourl(APIView):
-#     def delete(self, request, pk):
-#         # id = request.id
-#         snippet = urlModel.get_object(pk =pk)
-#         snippet.delete()
-#         return {}
-
-
-# class delblogdata(APIView):
-#     def delete(self, request):
-#         id = request.id
-#         blogMOdel.objects.get(pk =id).delete()
-#         return {}
-
-
-# class delcontactData(APIView):
-#     def delete(self, request):
-#         id = request.id
-#         snippet = contactModel.objects.get(pk =id)
-#         snippet.delete()
-#         return {}
-
-
-
-# class blogdatadetails(APIView):
-#     def get(self, request):
-#         id =  request.id
-#         product = blogMOdel.objects.get(pk=id)
-#         serializer = blogSerializer(product, many =True)
-#         return Response(serializer.data)
-
-# ############################################################################
-
-
-
-
-
-
-# # class getserializerproducts(APIView):
-# #     def get(self, request, pk):
-# #         product = ProductModel.objects.all()
-# #         product = product.filter(serializer = pk)
-# #         serializer = SellerProductSerializer(product, many =True)
-
-# #         return Response(serializer.data)
-
-
-
-
-# # class blogdata(APIView):
-# #     def post(self, request):
-# #         serializer = blogSerializer(data=request.data)
-# #         serializer.is_valid(raise_exception=True)
-# #         serializer.save()
-# #         return Response(serializer.data)
-    
-
-
-# # class contactData(APIView):
-# #     def post(self, request):
-# #         serializer = contactSerializer(data=request.data)
-# #         serializer.is_valid(raise_exception=True)
-# #         serializer.save()
-# #         return Response(serializer.data)
-    
+    elif request.method == 'DELETE':
+        contactModel.objects.get(pk=pk).delete()
+        return Response(status = 200)
