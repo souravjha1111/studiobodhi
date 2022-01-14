7c0e5907f00dc2994943b3ac42c0bbb1e6c2b455
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-01-14 20:19:00 +0530

    Update urls.py

--- studiobodhi/api/urls.py
+++ studiobodhi/api/urls.py
@@ -4,6 +4,7 @@ from api import views
 urlpatterns = [
     url('photo-url/(?P<pk>[0-9]+)', views.get_post_pic, name='getPhotoUrl'),
     url('blog-data/(?P<pk>[0-9]+)', views.blogdata, name='blogData'),
+    url('data/(?P<pk>[0-9]+)', views.data, name='getblogData'),
     url('contact-save/(?P<pk>[0-9]+)', views.contactsave, name='contactData'),
 
 ]
